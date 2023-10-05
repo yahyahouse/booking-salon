bf5f5f5e86d74357ea0fab0912ea6566987ac9bf
M Hilman Fahmi <mhifahmi02@gmail.com>
2023-10-05 08:40:42 +0700

    Add file

---
+++ src/main/java/com/booking/service/ReservationService.java
@@ -0,0 +1,18 @@
+package com.booking.service;
+
+
+public class ReservationService {
+    public static void createReservation(){
+
+    }
+
+    public static void getCustomerByCustomerId(){
+        
+    }
+
+    public static void editReservationWorkstage(){
+        
+    }
+
+    // Silahkan tambahkan function lain, dan ubah function diatas sesuai kebutuhan
+}
